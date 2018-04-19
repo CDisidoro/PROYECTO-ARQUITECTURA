230e721b24c8526015303c625a5c316e097b17c5
Camilo Andres D'isidoro Flechas <camilodisidoro2015@gmail.com>
2018-04-19 15:07:59 -0500

    uno que otro detalle

--- game.php
+++ game.php
@@ -2,43 +2,26 @@
 session_start(); //abrir una sesion 
 require_once('conexion.php'); 
 if(isset($_SESSION['ingreso']) && $_SESSION['ingreso'] ==true){ 
- 
-} 
- 
+}
 else{ 
   echo "<!DOCTYPE html> 
-<html lang='en'> 
-  <head> 
-  <meta charset='UTF-8'> 
-  <link rel='stylesheet' type='text/css' href='css/estilos.css'> 
-  <link rel='stylesheet' type='text/css' href='estilos.css'> 
-  <link rel='stylesheet' type='text/css' href='cabeza.css'>
-  <link rel='icon' type='image/png' href='src/login.png'>
-  <title>ACCESO ILEGAL</title>
-</head> 
-<body>  
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <center><h1 style='font-size: 60px; font-family: Iceland;'>ACCESO ILEGAL AL JUEGO</h1>
-  <center><h1 style='font-size: 60px; font-family: Iceland;'>POR FAVOR, INICIE SESIÓN DE FORMA ADECUADA</h1>
-  <a href='index.html' style='font-size: 60px; color:black;font-size: 40px;text-decoration: none;font-weight: bold;font-family: Iceland;'>Volver a Inicio</a>
-  </center>
- </body> 
-</html>"; 
+  <html lang='en'> 
+    <head> 
+    <meta charset='UTF-8'> 
+    <link rel='stylesheet' type='text/css' href='css/estilos.css'> 
+    <link rel='stylesheet' type='text/css' href='estilos.css'> 
+    <link rel='stylesheet' type='text/css' href='cabeza.css'>
+    <link rel='icon' type='image/png' href='src/login.png'>
+    <title>ACCESO ILEGAL</title>
+  </head> 
+  <body>
+      <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <center><h1 style='font-size: 60px; font-family: Iceland;'>ACCESO ILEGAL AL JUEGO</h1>
+    <center><h1 style='font-size: 60px; font-family: Iceland;'>POR FAVOR, INICIE SESIÓN DE FORMA ADECUADA</h1>
+    <a href='index.html' style='font-size: 60px; color:black;font-size: 40px;text-decoration: none;font-weight: bold;font-family: Iceland;'>Volver a Inicio</a>
+    </center>
+   </body> 
+  </html>"; 
   exit; 
 } 
 ?><!DOCTYPE html> 
@@ -52,19 +35,7 @@ else{
   <title>Operations DC</title>
 </head> 
 <body>  
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
   </center>
  </body> 
 </html>
@@ -83,11 +54,8 @@ while($fila=$stmt->fetch()){
   $fila2 = $stmt2->fetch();
   echo '<center><img src= '.$fila['avatar']." width='8%'><font style='font-size: 60px; color:black;font-size: 40px;text-decoration: none;font-weight: bold;font-family: Iceland;'>USUARIO: ".strtoupper($nick)."<br>Su puntaje es: ".$fila2['Puntaje']."<br>Su puntaje fue obtenido el: ".$fila['Obtener_Fecha_Score']."</font></center>";
 }
-?> 
- 
- 
+?>
 <br> 
   <center><a href='cerrar_sesion.php' style='font-size: 60px; color:black;font-size: 40px;text-decoration: none;font-weight: bold;font-family: Iceland;'>Cerrar Sesión</a></center>
- 
 </body> 
 </html> 
--- validarusuario.php
+++ validarusuario.php
@@ -1,133 +1,73 @@
 <?php  
 session_start(); //abrir una sesion  
 ?> 
- 
-<?php  
-/* SQLI
-$db="bd_juego"; 
-$user = "root"; 
-$pass=""; 
-$host = "localhost"; 
- 
-$conexion = new mysqli($host, $user, $pass, $db); */
+<?php
 require_once('conexion.php'); 
 $nickname =$_POST['nickname']; 
 $password=$_POST['password']; 
- 
 $con = new Conexion(); 
 $colombia =$con->Conectar(); 
- 
 $sql = "SELECT * FROM usuario WHERE nickname = '$nickname'"; 
- 
-//$resultado = $conexion->query($sql); 
 $stmt = $colombia->prepare($sql); 
 $stmt->execute();
  if($stmt->rowCount() > 0){  
 $row = $stmt->fetch(); 
- 
   if($password==$row['password']){ 
- 
     $_SESSION['ingreso'] = true; 
     $_SESSION['nickname'] = $nickname; 
-   
 echo "<!DOCTYPE html> 
-<html lang='en'> 
-  <head> 
-  <meta charset='UTF-8'> 
-  <link rel='stylesheet' type='text/css' href='css/estilos.css'> 
-  <link rel='stylesheet' type='text/css' href='estilos.css'> 
-  <link rel='stylesheet' type='text/css' href='cabeza.css'>
-  <link rel='icon' type='image/png' href='src/login.png'>
-  <title>Iniciar Sesión</title>
-</head> 
-<body>  
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <center><h1 style='font-size: 60px; font-family: Iceland;'>BIENVENIDO :".strtoupper($nickname)."</h1>
-  <a href='game.php?id=".$nickname."' style='font-size: 60px; color:black;font-size: 40px;text-decoration: none;font-weight: bold;font-family: Iceland;'>Ingresar al Juego</a>
-  </center>
- </body> 
-</html>"; 
+	<html lang='en'> 
+	  <head> 
+	  <meta charset='UTF-8'> 
+	  <link rel='stylesheet' type='text/css' href='css/estilos.css'> 
+	  <link rel='stylesheet' type='text/css' href='estilos.css'> 
+	  <link rel='stylesheet' type='text/css' href='cabeza.css'>
+	  <link rel='icon' type='image/png' href='src/login.png'>
+	  <title>Iniciar Sesión</title>
+	</head> 
+	<body>  
+	  <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+	  <center><h1 style='font-size: 60px; font-family: Iceland;'>BIENVENIDO :".strtoupper($nickname)."</h1>
+	  <a href='game.php?id=".$nickname."' style='font-size: 60px; color:black;font-size: 40px;text-decoration: none;font-weight: bold;font-family: Iceland;'>Ingresar al Juego</a>
+	  </center>
+	 </body> 
+	</html>"; 
 }else{ 
     echo "<!DOCTYPE html> 
-<html lang='en'> 
-  <head> 
-  <meta charset='UTF-8'> 
-  <link rel='stylesheet' type='text/css' href='css/estilos.css'> 
-  <link rel='stylesheet' type='text/css' href='estilos.css'> 
-  <link rel='stylesheet' type='text/css' href='cabeza.css'>
-  <link rel='icon' type='image/png' href='src/login.png'>
-  <title>Iniciar Sesión</title>
-</head> 
-<body>    
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <center><h1 style='font-size: 60px; font-family: Iceland;'>CONTRASEÑA INCORRECTAs</h1>
-  <a href='index.html' style='font-size: 60px; color:black;font-size: 40px;text-decoration: none;font-weight: bold;font-family: Iceland;'>Volver a Inicio</a>
-  </center>
- </body> 
-</html>"; 
+	<html lang='en'> 
+	  <head> 
+	  <meta charset='UTF-8'> 
+	  <link rel='stylesheet' type='text/css' href='css/estilos.css'> 
+	  <link rel='stylesheet' type='text/css' href='estilos.css'> 
+	  <link rel='stylesheet' type='text/css' href='cabeza.css'>
+	  <link rel='icon' type='image/png' href='src/login.png'>
+	  <title>Iniciar Sesión</title>
+	</head> 
+	<body>  
+	  <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+	  <center><h1 style='font-size: 60px; font-family: Iceland;'>CONTRASEÑA INCORRECTAs</h1>
+	  <a href='index.html' style='font-size: 60px; color:black;font-size: 40px;text-decoration: none;font-weight: bold;font-family: Iceland;'>Volver a Inicio</a>
+	  </center>
+	 </body> 
+	</html>"; 
   } 
 }else{ 
     echo "<!DOCTYPE html> 
-<html lang='en'> 
-  <head> 
-  <meta charset='UTF-8'> 
-  <link rel='stylesheet' type='text/css' href='css/estilos.css'> 
-  <link rel='stylesheet' type='text/css' href='estilos.css'> 
-  <link rel='stylesheet' type='text/css' href='cabeza.css'>
-  <link rel='icon' type='image/png' href='src/login.png'>
-  <title>Iniciar Sesión</title>
-</head> 
-<body>    
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <center><h1 style='font-size: 60px; font-family: Iceland;'>USUARIO NO ENCONTRADO</h1>
-  <a href='index.html' style='font-size: 60px; color:black;font-size: 40px;text-decoration: none;font-weight: bold;font-family: Iceland;'>Volver a Inicio</a>
-  </center>
- </body> 
-</html>"; 
+	<html lang='en'> 
+	  <head> 
+	  <meta charset='UTF-8'> 
+	  <link rel='stylesheet' type='text/css' href='css/estilos.css'> 
+	  <link rel='stylesheet' type='text/css' href='estilos.css'> 
+	  <link rel='stylesheet' type='text/css' href='cabeza.css'>
+	  <link rel='icon' type='image/png' href='src/login.png'>
+	  <title>Iniciar Sesión</title>
+	</head> 
+	<body>   
+	  <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+	  <center><h1 style='font-size: 60px; font-family: Iceland;'>USUARIO NO ENCONTRADO</h1>
+	  <a href='index.html' style='font-size: 60px; color:black;font-size: 40px;text-decoration: none;font-weight: bold;font-family: Iceland;'>Volver a Inicio</a>
+	  </center>
+	 </body> 
+	</html>"; 
   } 
   ?>
