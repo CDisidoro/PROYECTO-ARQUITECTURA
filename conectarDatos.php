a6b3656ce1eb096dfbb0cddbf98ca0b59fcbae14
Camilo Andres D'isidoro Flechas <camilodisidoro2015@gmail.com>
2018-05-21 14:40:23 -0500

    mas joda

--- conectarDatos.php
+++ conectarDatos.php
@@ -1,10 +1,14 @@
 <?php
+session_start(); 
 require_once('conexion.php');
-
 $jugador = $_SESSION['nickname'];
-$puntaje = $_POST['puntos'];
-$tiempo = $_POST['tiempo'];
-
+if(isset($_POST['puntos']) && isset($_POST['tiempo'])){
+	$puntaje = $_POST['puntos'];
+	$tiempo = $_POST['tiempo'];
+}else{
+	$puntaje = 60;
+	$tiempo = 30;
+}
 $conex = new Conexion();
 $establecer = $conex->Conectar();
 
@@ -14,17 +18,19 @@ $stmt->execute();
 $datos = $stmt->fetch();
 $rank =$datos['Puntajes_Ranking'];//RANGO DEL USUARIO LOGEADO CON SU PUNTAJE CORRESPONDIENTE
 if($stmt->rowCount()>0){
-
-
 	$tablaComm = "SELECT * FROM puntajes WHERE Ranking = '$rank'"; //DATOS DE TABLA PUNTAJES
 	$tablaEjct = $establecer->prepare($tablaComm);
 	$tablaEjct->execute();
 	$datosP = $tablaEjct->fetch();
-
-	$sql2= "UPDATE puntajes SET Ranking = '$datosP['Ranking']',Puntaje='$puntaje',Tiempo='$tiempo' WHERE Ranking='$rank'"; 
+	$rankP = $datosP['Ranking'];
+	$sql2 = "UPDATE puntajes SET Ranking = '$rankP',Puntaje='$puntaje',Tiempo='$tiempo' WHERE Ranking='$rank' "; 
 	$stmt1= $establecer->prepare($sql2);
 	$stmt1->execute();
-
+	$fecha = date('Y-m-d');
+	echo $fecha;
+	$fechaUpdate = "UPDATE usuario SET Obtener_Fecha_Score = '$fecha' WHERE nickname = '$jugador'";
+	$fechaComm = $establecer->prepare($fechaUpdate);
+	$fechaComm->execute();
 }else{
 	$tablaComm = "SELECT * FROM puntajes WHERE Ranking = '$rank'";
 	$tablaEjct = $establecer->prepare($tablaComm);
